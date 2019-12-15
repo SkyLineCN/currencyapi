152ea0f934b7eea650c6dc5193cd570866382019
David Lee <1125023182@qq.com>
2019-12-15 15:39:00 +0800

    使用spring boot框架创建项目，并导入基本依赖。

---
+++ src/main/java/com/skyline/currencyapi/CurrencyApplication.java
@@ -0,0 +1,16 @@
+package com.skyline.currencyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 李星燃
+ */
+@SpringBootApplication
+public class CurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyApplication.class, args);
+	}
+
+}
